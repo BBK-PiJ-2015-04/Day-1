773f4ece98ca3c3f3497497c512ab87b6cff2777
HS <none@email.none>
2015-10-09 10:36:35 +0100

    .

--- 13_All_Primes_Up_To_1000.groovy
+++ 13_All_Primes_Up_To_1000.groovy
@@ -1,28 +1,20 @@
 int number = 1
-
-while (number < 20) {
-
-
-
-
-
-int i = 1
-int prime = 1
-int divTest
-while (i < number) {
-	i++;
-	divTest = number % i
-	if (divTest == 0) {
-		if (i < number) {
-			prime=0
-			break
+while (number < 1001) {
+	int i = 1
+	int prime = 1
+	int divTest
+	while (i < number) {
+		i++;
+		divTest = number % i
+		if (divTest == 0) {
+			if (i < number) {
+				prime=0
+				break
+			}
 		}
 	}
-}
-
-if (prime == 1) {
+	if (prime == 1) {
 	println number
-}
+	}
 number++
-
 }
--- 14_Up_To_1000_Primes.groovy
+++ 14_Up_To_1000_Primes.groovy
@@ -1,26 +1,26 @@
-
-
-
-
-print "Please enter a number "
-String str = System.console().readLine();
-int number = Integer.parseInt(str);
-int i = 1
-int prime = 1
-int divTest
-while (i < number) {
-	i++;
-	divTest = number % i
-	if (divTest == 0) {
-		if (i < number) {
-			prime=0
-			break
+int number = 1
+int counter = 0
+int dummy = 0
+while (dummy == 0) { // endless loop - the only way out is to break
+	int i = 1
+	int prime = 1
+	int divTest
+	while (i < number) {
+		i++;
+		divTest = number % i
+		if (divTest == 0) {
+			if (i < number) {
+				prime=0
+				break
+			}
 		}
 	}
-}
-if (prime == 1) {
-	println ("The number is a prime number.")
-}
-else {
-	println("The number is NOT a prime number!")
+	if (prime == 1) {
+	println number
+	counter++
+	}
+	if (counter == 1000) { // have we reached a thousand primes?
+	break
+	}
+number++
 }
