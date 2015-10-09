44364f14c4a37c38b8b3ed95c28eac8ce53cabf2
HS <none@email.none>
2015-10-09 12:07:20 +0100

    .

--- 15_Guess_My_Number.groovy
+++ 15_Guess_My_Number.groovy
@@ -1,10 +1,12 @@
 int mynumber = (999 * Math.random()) + 1 // Between 0 and 1000 is 1-999
 int guess = 0
+int counter = 0
 String str
 
 println "Try to guess my number!"
 
 while (guess != mynumber) {
+	counter++
 	print ("Tell me a number: ")
 	str = System.console().readLine()
 	guess = Integer.parseInt(str)
@@ -16,12 +18,7 @@ while (guess != mynumber) {
 	}
 	else {
 		println ("CORRECT!")
+		print ("You needed " + counter + " guesses.")
 		break
 	}
-
 }
-
-
-
-
-
